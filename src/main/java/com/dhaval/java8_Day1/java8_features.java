3169d6699304dac68e6d6126cdf26b38391027e0
Vacchani Dhaval Mahendar <dhavalvacchani@gmail.com>
2025-08-23 11:23:20 +0530

    This is my First Day1 Commit

---
+++ src/main/java/com/dhaval/java8_Day1/java8_features.java
@@ -0,0 +1,72 @@
+package com.dhaval.java8_Day1;
+
+public class java8_features {
+
+/*
+Java 1.8 V Features:::::::;
+====================================>
+
+1.Java 1.8 Introduced Lot of New features.
+2.Java 1.8v new features Changed Java Programming Style.
+
+
+=================
+Main Objectives of Java 1.8 v
+===================================>
+1.Simplify Java Programming.
+2.Enable Functional Programming.
+3.Write more readable and consice code (Less number of lines of code to do more work).
+
+============================================================================================================================================================================================================
+============================================================================================================================================================================================================
+============================================================================================================================================================================================================
+============================================================================================================================================================================================================
+
+Java 1.8 Features
+
+1.Interface changes (We can write the default or concrete methods also in interface)
+
+   1.1:: Default Methods
+   1.2:: Static methods.
+Interface:Means collection of abstract methods.
+
+
+2.They Provided Functional Interfaces.
+
+	2.1: Predicate and BiPredicate
+	2.2:: Consumer
+	2.3:: Supplier
+	2.4:: Function.  pcsf
+
+3.Lambda Expressions:  Anonymous functions no names,no return type,No modifier.
+4.Method References and Constructor References.
+System.out.println("hi");
+from java 8:: System.out:: println
+
+::  ->Method references
+Its an alternative of the lambda expressions
+
+5.Stream API. vvimp Concept  (It is used to process the data).
+	Example:: List of employees available in collection
+	1.Get emps whose salary is >=1,00,000.
+	2.Get emps whose salary is <=30,000 and  Working in Delhi Location.
+	3.Get emp email whose salary is >1,00,000 and working in hyderabad location.
+
+
+6.Optional Class(To Avoid NullPointer Exception).
+7.SplitIterator   (For parallel processing,to process collection parallely)
+8.String Joiner
+9.Collections Framework changes.
+10.Date and Time API
+
+11.Nashron engine
+12.I/O Stream changes (Files.lines(Path p)
+13.Base64 Encoding and Decoding.
+
+
+Abstract methods:Methods does not contains the body is called as abstract methods
+Concrete methods:Method which contains the body is called as Concrete Methods.
+*/
+
+
+}
