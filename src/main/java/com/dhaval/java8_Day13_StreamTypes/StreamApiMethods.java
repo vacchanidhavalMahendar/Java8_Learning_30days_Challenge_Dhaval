531e6af64b4d121af5ec1f4792797e305f55fd14
Vacchani Dhaval Mahendar <dhavalvacchani@gmail.com>
2025-09-04 09:35:39 +0530

    Day 13

---
+++ src/main/java/com/dhaval/java8_Day13_StreamTypes/StreamApiMethods.java
@@ -0,0 +1,31 @@
+package com.dhaval.java8_Day13_StreamTypes;
+
+public class StreamApiMethods {
+
+/*    1.Stream API Provided several methods to perform Operations on the Data.
+
+2.We can Divide the Stream API Methods into 2 types.
+
+	1.Intermediate Operational Methods.  :
+    Will not provide any results,they accept functional interface parameters,Always Returns new stream.
+
+	2.Terminal Operational Methods.
+
+
+
+1.Intermediate Operational Methods.  :
+
+    It will perfom Operations on the (stream) and It returns a new (Stream).
+
+    Example: filter() , map() etc
+
+
+    filter:filter the data  or check some boolean condition
+
+
+    Terminal Operational Methods will take input and will provide result as (Output).
+
+
+    Example:count()  ; //it will just take data and give result as the Output.*/
+
+}
