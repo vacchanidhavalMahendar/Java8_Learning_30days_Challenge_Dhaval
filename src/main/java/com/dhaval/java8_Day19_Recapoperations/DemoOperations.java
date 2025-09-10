57451c9d7e9ccb9c0b03c8d88c758b4d83c10231
Vacchani Dhaval Mahendar <dhavalvacchani@gmail.com>
2025-09-10 21:01:42 +0530

    Day 19

---
+++ src/main/java/com/dhaval/java8_Day19_Recapoperations/DemoOperations.java
@@ -0,0 +1,25 @@
+package com.dhaval.java8_Day19_Recapoperations;
+
+public class DemoOperations {
+
+
+
+    /*Set 1:Intermediate Operations Method,Do operations and returns the stream
+
+    filters: filter()
+    mapping :map(), flatmap()
+    slicing: distinct() limit() skip()
+
+
+    FM
+            DLS
+
+2.Mapping Operations are used to (Transform or Conversion) the stream elements  and returns  transformed elements as (new stream)
+            =======================================================================================================>
+    Set 2:Terminal Operations Method,Do operations and returns the stream
+
+
+    finding:: findFirst(), findAny()
+    Matching: anyMatch(), allMatch(), noneMatch()
+    Collecting: collect(Collectors.toList());*/
+}
